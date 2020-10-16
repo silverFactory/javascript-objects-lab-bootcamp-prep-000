0153c3edf250fe91906c44a56f3e0eabd564d601
Will Studabaker <will.studabaker@gmail.com>
2020-10-16 19:24:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
   return object
 }
+function deleteFromObjectByKey(object, key){
+  let newObject = Object.assign({}, object);
+  delete newObject[key]
+}
